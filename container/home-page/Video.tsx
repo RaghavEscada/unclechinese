259a1696a68604b3de69a2db8b3fafb8bddb3f3b
Raghav Krishna <raghav@Raghavs-MacBook-Air.local>
2025-08-22 15:38:12 +0530

    .

--- container/home-page/Video.tsx
+++ container/home-page/Video.tsx
@@ -1,4 +1,4 @@
-import { OptimizedVideo } from "@/components/OptimizedVideo";
+import OptimizedVideo from "@/components/OptimizedVideo";
 
 export default function Video() {
 	return <OptimizedVideo videosrc="/herobg.mp4" priority={true} autoPlay={false} />;
